0359307d1f9de92f24cb77572b608f4a88c8b2ed
Vladislav <geowl@vk.com>
2023-08-10 14:53:34 +0300

    добавлено решение задачи task013 из домашнего задания

---
+++ task013HW/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+
+if (number >= 100)
+{
+    result = (number / 100) % 10;
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+    return;
+}
+
+    Console.WriteLine($"Третья цифра числа = {result}");
+
+
+
+
