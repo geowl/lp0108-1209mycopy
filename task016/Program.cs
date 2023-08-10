c37ba336908e43851696724438063806b4988358
Vladislav <geowl@vk.com>
2023-08-10 14:52:24 +0300

    добавлены новые задачи которые были решены на 2 семинаре по с#, задачи 09,11,12,14,16

---
+++ task016/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+// 5, 25 - > Да
+// -4, 16 - > Да
+// 25, 5 - > Да
+// 8, 9 - >  Нет
+
+bool CheckSquare(int num1, int num2)
+{
+    return num1 == num2 * num2 || num2 == num1 * num1;
+}
+Console.WriteLine("Введите два числа");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+
+bool result = CheckSquare(firstNumber, secondNumber);
+Console.WriteLine(result ? "Да" : "Нет");
---
+++ task09/Program.cs
@@ -0,0 +1,29 @@
+﻿//  Напишите программу, которая выводит случайное число из отрезка [10,99] и показывает наибольшую цифру числа
+
+int number = new Random().Next(10, 100); //99+1
+Console.WriteLine($"Случайное число в диапозоне 10 - 99 -> {number}");
+// int firstDigit = number / 10; // 78/10 = 7.8 -> 7
+// int secondDigit = number % 10; // 78%10 =  70 + 8 
+
+// // if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа -> {firstDigit}");
+// // else Console.WriteLine($"Наибольшая цифра числа -> {secondDigit}");
+
+// int result = firstDigit > secondDigit ? firstDigit : secondDigit; 
+// Console.WriteLine($"Наибольшая цифра числа -> {result}");
+
+int maxDigit = MaxDigit(number);   
+Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
+
+int maxDigit1 = MaxDigit(56);   
+Console.WriteLine($"Наибольшая цифра числа -> {maxDigit1}");
+
+int maxDigit2 = MaxDigit(89);   
+Console.WriteLine($"Наибольшая цифра числа -> {maxDigit2}");
+
+
+int MaxDigit(int num) // num = number (значения!); num = 93, num = 56, num 89 ...
+{
+    int firstDigit = num / 10;
+    int secondDigit = num / 10;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
