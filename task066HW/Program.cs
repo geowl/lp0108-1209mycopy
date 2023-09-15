222d5e8e17d42a0ec26fecfcb44cfe2a62d66969
Vladislav <geowl@vk.com>
2023-09-15 23:27:57 +0300

    добавлены последнии задачи из дз по рекурсии 64 66 68

---
+++ task066HW/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumNaturalNumbers(int from, int to)
+{
+    int num = 0;
+    int sum = 0;
+    if (from > 0 && to > 0)
+    {
+        for (int i = from; i <= to; i++)
+        {
+            num = i;
+            sum += num;
+        }
+    }
+    return sum;
+
+}
+
+Console.WriteLine("Введите диапозон натуральных чисел");
+Console.Write("Введите первое число: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+int result = SumNaturalNumbers(numberOne, numberTwo);
+Console.WriteLine($"М = {numberOne}; N = {numberTwo}. -> {result}");
---
+++ task068HW/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+// функция ack(n, m)
+//    если n = 0
+//      вернуть m + 1
+//    иначе, если m = 0
+//      вернуть ack (n - 1, 1)
+//    еще
+//      вернуть ack(n - 1, ack (n, m - 1))
+
+
+int AccermanFunction(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return AccermanFunction(m - 1, 1);
+    }
+    else
+    {
+        return (AccermanFunction(m - 1, AccermanFunction(m, n - 1)));
+    }
+}
+
+
+Console.WriteLine("Введите диапозон чисел.!!! ВАЖНО, ТЕХНИЧЕСКИ ВЫВОД ОГРАНИЧЕН, m ДО 3, n ДО 10 !!! ");
+Console.Write("Введите число m: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+if (numberOne > 3 || numberTwo > 10)
+{
+    Console.WriteLine("Ошибка!");
+}
+
+int result = AccermanFunction(numberOne, numberTwo);
+Console.WriteLine($"m = {numberOne}; n = {numberTwo}. -> A(m,n) = {result}");
